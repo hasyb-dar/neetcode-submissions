8bddda696185b730551cc0228a2bbfc7ab536ee0
Haseeb-dar <hasybfayaz@gmail.com>
2026-09-10 17:34:22 +0530

    Add: find-duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-5.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+
+        // n = max possible value in array
+        int n = nums.length - 1;
+
+        int low = 1;
+        int high = n;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            // STEP 1: count how many numbers are <= mid
+            int count = 0;
+
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+
+            // STEP 2: decision making
+            // If count > mid → duplicate lies in [1..mid]
+            if (count > mid) {
+                high = mid-1;
+            }
+            // else duplicate lies in [mid+1..n]
+            else {
+                low = mid + 1;
+            }
+        }
+
+        // low == high → duplicate number
+        return low;
+    }
+}
